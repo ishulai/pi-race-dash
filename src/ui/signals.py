43f81fb82030bcaac04a2dc7ef28223d1c7d061e
Thomas Lai <thomas@thomass-mbp-2.lan>
2024-10-23 10:55:28 -0700

    Modify signal colors

--- src/ui/signals.py
+++ src/ui/signals.py
@@ -1,19 +1,22 @@
 import tkinter as tk
 
+off_color = "black"
+on_color = "green"
+
 def render_left_signal(root, x, y):
     canvas = tk.Canvas(root, width=80, height=60, bg="black", highlightthickness=0)
     canvas.place(x=x, y=y)
-    arrow = canvas.create_polygon([20, 30, 50, 10, 50, 20, 70, 20, 70, 40, 50, 40, 50, 50], fill="gray", outline="gray")
+    arrow = canvas.create_polygon([20, 30, 50, 10, 50, 20, 70, 20, 70, 40, 50, 40, 50, 50], fill=off_color, outline=off_color)
     return canvas, arrow
 
 def render_right_signal(root, x, y):
     canvas = tk.Canvas(root, width=80, height=60, bg="black", highlightthickness=0)
     canvas.place(x=x, y=y)
-    arrow = canvas.create_polygon([60, 30, 30, 10, 30, 20, 10, 20, 10, 40, 30, 40, 30, 50], fill="gray", outline="gray")
+    arrow = canvas.create_polygon([60, 30, 30, 10, 30, 20, 10, 20, 10, 40, 30, 40, 30, 50], fill=off_color, outline=off_color)
     return canvas, arrow
 
 def update_signal(canvas, arrow, on):
     if on:
-        canvas.itemconfig(arrow, fill="green", outline="green")
+        canvas.itemconfig(arrow, fill=on_color, outline=on_color)
     else:
-        canvas.itemconfig(arrow, fill="gray", outline="gray")
+        canvas.itemconfig(arrow, fill=off_color, outline=off_color)
