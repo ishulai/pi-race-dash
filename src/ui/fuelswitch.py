cf171209112632ddc7ac53dba66cc22f23a21fe6
Thomas Lai <thomas@thomass-mbp-2.lan>
2024-11-01 22:00:49 -0700

    Fix image path

--- src/ui/fuelswitch.py
+++ src/ui/fuelswitch.py
@@ -1,10 +1,17 @@
+import os
 import tkinter as tk
 from .helpers import load_svg_to_image
+
+simulation_mode = os.environ.get("SIMULATION_MODE") != None
 
 def render_low_fuel_symbol(root, x, y):
     canvas = tk.Canvas(root, width=60, height=80, bg="black", highlightthickness=0)
     canvas.place(x=x, y=y)
-    fuel_image = load_svg_to_image("src/ui/assets/fuel.svg")
+    if simulation_mode:
+        image_path = "src/ui/assets/fuel.svg"
+    else:
+        image_path = "pi-race-dash/src/ui/assets/fuel.svg"
+    fuel_image = load_svg_to_image(image_path)
     fuel_image_item = canvas.create_image(40, 40, image=fuel_image)
 
     return canvas, fuel_image_item, fuel_image
